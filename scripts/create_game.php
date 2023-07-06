34f78a5ef982220effbf905c67bc5a4c1c8ef1b9
Sam Hopkins <shopkins@coretechs.com>
2023-07-06 16:55:30 -0400

    first draft create_game php script

--- scripts/create_game.php
+++ scripts/create_game.php
@@ -3,10 +3,10 @@ require 'connect.php';
 
 $game_type = $_POST['game_type'];
 
-if ($game_type == 'countdown') {
+if ($game_type == 'Countdown') {
     $starting_points = $_POST['count_select'];
 }
-elseif ($game_type == 'highscore') {
+elseif ($game_type == 'Highscore') {
     $number_of_rounds = $_POST['round_select'];
 }
 
@@ -17,6 +17,19 @@ for ($i = 1; $i <= $player_count; $i++) {
 
     $player_names[] = $player_name;
 }
+
+$players_json = json_encode($player_names);
+
+if ($game_type == 'Countdown') {
+    $sql = "INSERT INTO 'game_data' ('type','starting_points','player_count','players','time') 
+        VALUES ('$game_type','$starting_points','$player_count','$players_json','NOW())";
+}
+elseif ($game_type == 'Highscore') {
+    $sql = "INSERT INTO 'game_data' ('type','number_of_rounds','player_count','players','time') 
+        VALUES ('$game_type','$number_of_rounds','$player_count','$players_json','NOW())";
+}
+
+
 
 $conn->close();
 ?>
