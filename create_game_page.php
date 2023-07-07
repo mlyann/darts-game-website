80f1913d54c179bc82fafb0484026583a6d5d9ec
Sam Hopkins <shopkins@coretechs.com>
2023-07-07 15:52:22 -0400

    make create game fields required

--- create_game_page.php
+++ create_game_page.php
@@ -39,7 +39,7 @@
     <h1>Create Game</h1>
     <form id ="create_game_form" action ="scripts/create_game.php" method = "POST">
     <label for="game_type">Game Type:</label>
-    <select name="game_type" id="game_type" onchange="showConditionalCreateOption()">
+    <select name="game_type" id="game_type" onchange="showConditionalCreateOption()" required>
         <option value="">Select Type</option>
         <option value="Countdown">Count Down</option>
         <option value="Highscore">High Score</option>
@@ -48,7 +48,7 @@
 
     <div id="count_form" style="display:none">
     <label for="count_select" id ="count_label"></label>
-    <select id="count_select" name="count_select">
+    <select id="count_select" name="count_select" required>
         <option value="301">301</option>
         <option value="501">501</option>
     </select>
@@ -57,7 +57,7 @@
 
     <div id="highscore_form" style="display:none">
     <label for="round_select" id ="round_label"></label>
-    <select id="round_select" name="round_select">
+    <select id="round_select" name="round_select" required>
         <option value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
@@ -69,7 +69,7 @@
 
     <div name="player_form">
         <label for="player_count">Select Number of Players (1-6):</label>
-        <select name="player_count" id="player_count" onchange="showPlayerForm()">
+        <select name="player_count" id="player_count" onchange="showPlayerForm()" required>
             <option value="">Select Number</option>
             <option value="1">1</option>
             <option value="2">2</option>
