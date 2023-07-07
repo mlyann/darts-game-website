58c4491532caf3eba47a2d8e5014311f39fafe0a
Sam Hopkins <shopkins@coretechs.com>
2023-07-07 10:38:36 -0400

    create homepage, rename register for clarity

---
+++ register_page.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Registration</title>
+</head>
+<body>
+    <h1>User Registration</h1>
+
+    <form action="scripts/adduser.php" method="POST">
+        <label for="name">Name:</label>
+        <input type="text" name="name" id="name" required>
+        <br><br>
+        <input type="submit" value="Register">
+    </form>
+
+</body>
+</html>
+
+
+
