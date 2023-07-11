2761e03162702ca35fe9e298a141736159ccff7b
Sam Hopkins <shopkins@coretechs.com>
2023-07-11 15:39:45 -0400

    improve sql, still not working, sends 0 always

--- scripts/updateTurnScores.php
+++ scripts/updateTurnScores.php
@@ -1,22 +1,37 @@
 <?php
-              require 'connect.php';
+    require 'connect.php';
 
-              $turnScores = json_decode($_POST['turnScores']);
-              $name = $_POST['name'];
-              
-              $query = "UPDATE scores 
+    $turn1 = $_POST['turn1'];
+    $turn2 = $_POST['turn2'];
+    $turn3 = $_POST['turn3'];
+    $name = $_POST['name'];
+
+    // Escape variables to prevent SQL injection
+    $turn1 = mysqli_real_escape_string($conn, $turn1);
+    $turn2 = mysqli_real_escape_string($conn, $turn2);
+    $turn3 = mysqli_real_escape_string($conn, $turn3);
+    $name = mysqli_real_escape_string($conn, $name);
+
+    // Get the maximum turn value for the given name
+    $subQuery = "SELECT MAX(turn) FROM scores WHERE Name = '$name'";
+    $result = mysqli_query($conn, $subQuery);
+    $row = mysqli_fetch_assoc($result);
+    $maxTurn = $row['MAX(turn)'];
+
+    // Update the scores table
+    $query = "UPDATE scores 
               SET 
-                first = turnScores[0],
-                second = turnScores[1],
-                third = turnScores[2]
-              WHERE Name = $name
-                AND turn = (SELECT MAX(turn) FROM scores WHERE Name = $name)";
+                first = '$turn1',
+                second = '$turn2',
+                third = '$turn3'
+              WHERE Name = '$name'
+                AND turn = $maxTurn";
 
-            $result = mysqli_query($conn, $query);
+    $result = mysqli_query($conn, $query);
 
-            if(!$result){
-                echo "Error updating turn scores:" . mysqli_error($conn);
-            }
-            
-            $conn->close();
+    if (!$result) {
+        echo "Error updating turn scores: " . mysqli_error($conn);
+    }
+
+    $conn->close();
 ?>
