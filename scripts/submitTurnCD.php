dfe2d4424912e82a8cd51e6076c9b0caadfc0f7e
Sam Hopkins <shopkins@coretechs.com>
2023-07-19 16:32:13 -0400

    fix typo

--- scripts/submitTurnCD.php
+++ scripts/submitTurnCD.php
@@ -57,8 +57,8 @@
   if ($overall == 0 && $lastMult == 2) {
     $sql = "INSERT INTO wins (name, time) VALUES ('$currentPlayer', NOW())";
     mysqli_query($conn, $sql);
-    $sql = "INSERT INTO scores (name, overall) VALUES ('$currentPlayer', -353";
-    mysqli_query($conn,$sql);
+    $sql = "INSERT INTO scores (name, overall) VALUES ('$currentPlayer', -353)";
+    mysqli_query($conn, $sql);
     //TODO end the game
   }//bust
   else if ($overall <= 0) {
