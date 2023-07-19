f94029b7b69fcb266b6b02c5840433a9417475c1
Sam Hopkins <shopkins@coretechs.com>
2023-07-19 00:00:32 -0400

    add and implement tracking of currentplayer on scoring page

---
+++ scripts/getCurrentPlayer.js
@@ -0,0 +1,20 @@
+function getCurrentPlayer() {
+    $.ajax({
+      url: 'scripts/getCurrentPlayer.php',
+      method: 'GET',
+      success: function(response) {
+        // Process the response
+        updateTableCell('currentPlayerCell', response);
+  
+        // Call the function again after 1 second
+        setTimeout(getCurrentPlayer, 1000);
+      },
+      error: function(xhr, status, error) {
+        // Handle error
+        console.error(error);
+  
+        // Call the function again after 1 second
+        setTimeout(getCurrentPlayer, 1000);
+      }
+    });
+  }
