d19a09629a081419b654ced18b442e2a25d2b1a0
Sam Hopkins <shopkins@coretechs.com>
2023-07-19 20:07:12 -0400

    add scoring.php to homepage

--- homepage.php
+++ homepage.php
@@ -9,9 +9,10 @@
     <h2>Navigation:</h2>
     <ul>
         <li><a href="leaderboard_page.php">Leaderboard</a></li>
-        <li><a href="register_page.php">Register/Unregister A Player!</a></li>
+        <li><a href="register_page.php">Register/Deregister A Player</a></li>
         <li><a href="create_game_page.php">Create a Game!</a></li>
-        <li><a href="scoreDisplayPage.php">View Game Score</a></li>
+        <li><a href="scoreDisplayPage.php">View Live Game Score</a></li>
+        <li><a href="scoring.php">Participate in Current Game</a></li>
     </ul>
 </body>
 </html>
