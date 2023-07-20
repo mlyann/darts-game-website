3e9491e042141075d253c9f9821f8223ec413099
Sam Hopkins <shopkins@coretechs.com>
2023-07-20 16:47:43 -0400

    add script to generate info rows on scoring page for players

---
+++ scripts/fetch_names_scoring.php
@@ -0,0 +1,30 @@
+<?php
+    require 'connect.php';
+
+    //get players in the game
+    $sql = "SELECT players from game_data";
+    $players = mysqli_query($conn, $sql);
+    $row = $players->fetch_assoc();
+    $playersArray = json_decode($row['players'], true);
+
+    $playerIndex = 1;
+    
+    foreach ($playersArray as $player) {
+        $id = $playerIndex . "nameCell";
+        $overallCell = $playerIndex . "overallCell";
+        $firstCell = $playerIndex . "firstCell";
+        $secondCell = $playerIndex . "secondCell";
+        $thirdCell = $playerIndex . "thirdCell";
+        echo '<tr>';
+        echo '<td id="' . $id . '"></td>';
+        echo '<td id="' . $overallCell . '"></td>';
+        echo '<td id="' . $firstCell . '"></td>';
+        echo '<td id="' . $secondCell . '"></td>';
+        echo '<td id="' . $thirdCell . '"></td>';
+        echo '</tr>';
+        $playerIndex += 1;
+        
+    }
+
+    $conn->close();
+?>
