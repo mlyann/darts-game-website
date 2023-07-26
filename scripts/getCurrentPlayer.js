e957996012bfd7d62c53689fc371059dfcce9695
Sam Hopkins <shopkins@coretechs.com>
2023-07-26 10:22:58 -0400

    wins tracking cell will be properly highlighted and unhighlighted as currentPlayer changes

--- scripts/getCurrentPlayer.js
+++ scripts/getCurrentPlayer.js
@@ -6,23 +6,29 @@ function getCurrentPlayer() {
       //set index to index of currentplayer, increment index to id cells 
       index = response;
       index++;
-
       
       highlightColor = 'red';
       defaultColor = '#efefef';
 
-      
       const currentPlayerNameCell = document.getElementById(index + 'nameCell');
       const currentPlayerOverallCell = document.getElementById(index + 'overallCell');
       const currentPlayerFirstCell = document.getElementById(index + 'firstCell');
       const currentPlayerSecondCell = document.getElementById(index + 'secondCell');
       const currentPlayerThirdCell = document.getElementById(index + 'thirdCell');
 
+      let currentPlayerWinsCell = null;
+      if (gamemode == 'Highscore') {
+        currentPlayerWinsCell = document.getElementById(index + 'winsCell');
+      }
+
       currentPlayerNameCell.style.backgroundColor = highlightColor;
       currentPlayerOverallCell.style.backgroundColor = highlightColor;
       currentPlayerFirstCell.style.backgroundColor = highlightColor;
       currentPlayerSecondCell.style.backgroundColor = highlightColor;
       currentPlayerThirdCell.style.backgroundColor = highlightColor;
+      if (currentPlayerWinsCell) {
+        currentPlayerWinsCell.style.backgroundColor = highlightColor;
+      }
 
       for (let i = 1; i <= 6 ; i++) {
         if (document.getElementById(i + 'nameCell') === null) { break; }
@@ -33,6 +39,9 @@ function getCurrentPlayer() {
             document.getElementById(i + 'firstCell').style.backgroundColor = defaultColor;
             document.getElementById(i + 'secondCell').style.backgroundColor = defaultColor;
             document.getElementById(i + 'thirdCell').style.backgroundColor = defaultColor;
+            if (gamemode == 'Highscore') {
+              document.getElementById(i + 'winsCell').style.backgroundColor = defaultColor;
+            }
           }
         }
       }
