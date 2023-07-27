80994afd23fd19a47b3e0073588fa2180cd160bc
Daniel Wang <dwang@coretechs.com>
2023-07-27 13:15:38 -0400

    update getCurrentPlayer

--- scripts/getCurrentPlayer.php
+++ scripts/getCurrentPlayer.php
@@ -13,15 +13,36 @@ $query = "SELECT currentPlayer FROM game_data";
 $result = $conn->query($query);
 
 // Fetch the data
-$data = "";
+$currentPlayer = "";
 if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
-    $data = $row['currentPlayer'];
+    $currentPlayer = $row['currentPlayer'];
 }
+
+//get index of currentplayer
+$query = "SELECT players FROM game_data";
+$result = mysqli_query($conn, $query);
+
+if (!$result) {
+    echo "Error getting players: " . mysqli_error($conn);
+}
+
+$playerNames = [];
+
+while ($row = $result->fetch_assoc()) {
+    $jsonNames = $row['players'];
+    $namesArray = json_decode($jsonNames, true);
+    
+    if (is_array($namesArray)) {
+        $playerNames = array_merge($playerNames, $namesArray);
+    }
+}
+
+$currentPlayerIndex = array_search($currentPlayer, $playerNames);
 
 // Close the database connection
 $conn->close();
 
 // Return the data as the response
-echo $data;
+echo $currentPlayerIndex;
 ?>
