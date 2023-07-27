ee8d51874990e39aed47d5033dba4be04ea4ef0f
Daniel Wang <dwang@coretechs.com>
2023-07-27 13:21:12 -0400

    update updateTableCell

--- scripts/updateTableCell.js
+++ scripts/updateTableCell.js
@@ -1,4 +1,5 @@
 function updateTableCell(id, data) {
-    const tableCell = document.getElementById(id);
+  const tableCell = document.getElementById(id);
+  if(tableCell != null)
     tableCell.innerHTML = data;
-  }
+}
