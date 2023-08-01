cde49d6567cc6ffd1b891dc6164035db8b1f844b
Sam Hopkins <shopkins@coretechs.com>
2023-08-01 17:20:29 +0000

    multiplier resets on dart input

--- scripts/multiplier.js
+++ scripts/multiplier.js
@@ -31,24 +31,6 @@ function multiplier(value) {
 
   }
   else { //toggle off
-    multiplierActive = false;
-    if (multiplierValue == 2) {
-      doubleButton.classList.remove ('double');
-
-      bullButton.classList.remove('double');
-
-      for (let i = 0; i < buttons.length; i++) {
-        buttons[i].classList.remove('double');
-      }
-    }
-    else {
-      tripleButton.classList.remove('triple');
-
-      for (let i = 0; i < buttons.length; i++) {
-        buttons[i].classList.remove('triple');
-      }
-    }
-
-    multiplierValue = 1;
+    resetMultipliers();
   }
 }
