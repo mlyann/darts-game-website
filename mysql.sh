c7a8edce77a875c15ab9483129e637bb298c14bf
Ubuntu <ubuntu@ip-172-30-0-44.ec2.internal>
2023-08-03 15:06:36 +0000

    update sql login script

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # MySQL database credentials
-DB_HOST="darts-dev.co4398xc5nn7.us-east-1.rds.amazonaws.com"
+DB_HOST="darts-daniel-dev.co4398xc5nn7.us-east-1.rds.amazonaws.com"
 DB_PORT="3306"
 DB_USER="admin"
 DB_PASS="Bz5gfz^iLD0o7U.[?C8e"
