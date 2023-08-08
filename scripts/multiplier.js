c19fb60a9491b33865731cb1e385c3a84a6b2bd4
Sam Hopkins <shopkins@coretechs.com>
2023-08-08 17:41:29 +0000

    allows direct switching between multipliers and change indicator stylng

--- scripts/multiplier.js
+++ scripts/multiplier.js
@@ -12,23 +12,34 @@ function multiplier(value) {
       tripleButton.classList.add('triple');
     }
 
-    //change colors of input buttons
-    if (multiplierValue === 2) {
-      for (let i = 0; i < buttons.length; i++) {
-        buttons[i].classList.add('double');
-      }
-    }
-    else {
-      for (let i = 0; i < buttons.length; i++) {
-        buttons[i].classList.add('triple');
-      }
-    }
+    // //change colors of input buttons
+    // if (multiplierValue === 2) {
+    //   for (let i = 0; i < buttons.length; i++) {
+    //     buttons[i].classList.add('double');
+    //   }
+    // }
+    // else {
+    //   for (let i = 0; i < buttons.length; i++) {
+    //     buttons[i].classList.add('triple');
+    //   }
+    // }
 
-    //deal with bullseye
-    if (multiplierValue === 2) {
-      bullButton.classList.add('double');
-    }
+    // //deal with bullseye
+    // if (multiplierValue === 2) {
+    //   bullButton.classList.add('double');
+    // }
 
+  }
+  else if (multiplierValue === 2 && value === 'triple') {
+    multiplierValue = 3;
+    tripleButton.classList.add('triple');
+    doubleButton.classList.remove('double');
+
+  }
+  else if (multiplierValue === 3 && value === 'double') {
+    multiplierValue = 2;
+    doubleButton.classList.add('double');
+    tripleButton.classList.remove('triple');
   }
   else { //toggle off
     resetMultipliers();
