295df667a780d19af665d685faf8b2788c9cf01b
Sam Hopkins <shopkins@coretechs.com>
2023-08-11 13:20:59 +0000

    remove unused script and fix bug on scoreDisplayPage

--- scoreDisplayPage.php
+++ scoreDisplayPage.php
@@ -2,74 +2,80 @@
 <html>
 <head>
 <meta name="viewport" content="user-scalable=no">
-    <title>Score Display Page</title>
-    <link rel="stylesheet" type="text/css" href="styles/scoreDisplayPage.css">
-    <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script type="text/javascript" src="scripts/getCurrentPlayer.js"></script>
-    <script type="text/javascript" src="scripts/updateTableCell.js"></script>
-    <script type="text/javascript" src="scripts/displayNames.js"></script>
-    <script type="text/javascript" src="scripts/displayInfo.js"></script>
+  <title>Score Display Page</title>
+  <link rel="stylesheet" type="text/css" href="styles/scoreDisplayPage.css">
+  <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script type="text/javascript" src="scripts/getCurrentPlayer.js"></script>
+  <script type="text/javascript" src="scripts/updateTableCell.js"></script>
+  <script type="text/javascript" src="scripts/displayNames.js"></script>
+  <script type="text/javascript" src="scripts/displayInfo.js"></script>
+  <script type="text/javascript" src="scripts/displayInfoCD.js"></script>
+  <script type="text/javascript" src="scripts/displayInfoHS.js"></script>
 
-    <script>
-        //get the gamemode
-        <?php
-          require 'scripts/connect.php';
+  <script>
+      //get the gamemode
+      <?php
+        require 'scripts/connect.php';
 
-          $query = "SELECT type FROM game_data";
-          $result = mysqli_query($conn, $query);
+        $query = "SELECT type FROM game_data";
+        $result = mysqli_query($conn, $query);
 
-          if ($result && $result->num_rows > 0) {
-            $row = $result->fetch_assoc();
-            $mode = $row['type'];
-          }
+        if ($result && $result->num_rows > 0) {
+          $row = $result->fetch_assoc();
+          $mode = $row['type'];
+        }
 
-          echo "var gamemode = '$mode';";
-          $conn->close();
-        ?>
+        echo "var gamemode = '$mode';";
+        $conn->close();
+      ?>
 
+    function displayInfo() {
+              if (gamemode == 'Countdown') {
+                displayInfoCD();
+              }
+              else if (gamemode = 'Highscore') {
+                displayInfoHS();
+              }
+            }
 
-        //populate info cells of table
-        getCurrentPlayer();
-        displayNames();
-        displayInfo();
+    //populate info cells of table
+    getCurrentPlayer();
+    displayNames();
+    displayInfo();
 
-
-
-
-function updateInfo(){
-getCurrentPlayer();
-displayInfo();
-setTimeout(updateInfo, 100);
-}
-updateInfo();
+      function updateInfo(){
+      getCurrentPlayer();
+      displayInfo();
+      setTimeout(updateInfo, 100);
+      }
+      updateInfo();
 
   </script>
   <style>
-.center {
-    margin-left: auto;
-    margin-right: auto;
-  }
-  
-  .button {
-    display: block;
-    width: 100%;
-    text-align: center;
-  }
-</style>
+    .center {
+        margin-left: auto;
+        margin-right: auto;
+      }
+      
+      .button {
+        display: block;
+        width: 100%;
+        text-align: center;
+      }
+  </style>
 </head>
-<body>
 
+<body>
 
 <div class = "center" id = "tableWrapper"> 
   <div class = "center" id = "infoContainer">
-<?php require 'scripts/generateTable.php' ?>
-<table class = "center gameInfo">
-  <tr>
-    <td class = "infoCell" id ="helpCell">D20 + D20 + D20</td>
-  </tr>
-</table>
-      </div>
-
+    <?php require 'scripts/generateTable.php' ?>
+    <table class = "center gameInfo">
+      <tr>
+        <td class = "infoCell" id ="helpCell">D20 + D20 + D20</td>
+      </tr>
+    </table>
+  </div>
 
 </body>
 </html>
