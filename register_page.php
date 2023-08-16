c14e364553955c10338dd023ce7c28b05cd3baa3
Sam Hopkins <shopkins@coretechs.com>
2023-08-16 15:56:58 +0000

    add basic styling to register page

--- register_page.php
+++ register_page.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta name="viewport" content="user-scalable=no">
     <title>User Management</title>
+    <link rel="stylesheet" type="text/css" href="styles/register_page.css">
     <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script>
         function showEditUser() {
@@ -23,6 +25,7 @@
     </script>
 </head>
 <body>
+    <div class = "registerContainer">
     <h1>User Registration</h1>
 
     <form action="scripts/adduser.php" method="POST">
@@ -32,7 +35,7 @@
         <label for="image">Image URL:</label>
         <input type="text" name="image" id="image">
         <br><br>
-        <input type="submit" value="Register">
+        <input class = "button" type="submit" value="Register">
     </form>
 
     <h1>User Deregistration</h1>
@@ -54,37 +57,42 @@
         ?>
     </select>
     <br><br>
-    <input type="submit" value="Deregister">
+    <input class = "button" type="submit" value="Deregister">
 </form>
 
 <h1>Edit User</h1>
 
-<form action="scripts/editUser.php" method="POST" onchange="showEditUser()" onsubmit="return confirm('Are you sure you want to edit this user?');>
+<form action="scripts/editUser.php" method="POST" onchange="showEditUser()" onsubmit="return confirm('Are you sure you want to edit this user?');">
     <label for="edit_select">Name:</label>
     <select name="edit_select" id="edit_select">
         <?php
         require 'scripts/connect.php';
-        $sql = "SELECT name FROM users";
+        $sql = "SELECT name, image_url FROM users";
         $result = $conn->query($sql);
         if ($result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
                 $name = $row['name'];
+                if (!$image_url) {
+                    $image_url = $row['image_url'];
+                }
                 echo '<option value="' . $name . '">' . $name . '</option>';
             }
         }
+        echo "<script>var defaultURL = '$image_url'</script>";
         $conn->close();
         ?>
     </select>
-    <img id="img" src = ""></img>
-    <br><br>
+    <img id="img" src = "https://www.coretechs.com/wp-content/uploads/2020/08/Coretechs_Mark.png"></img>
+    <script>document.getElementById('img').src = defaultURL;</script>
     <label for="edit_name">New Name:</label>
     <input type="text" name="edit_name" id="edit_name" required>
     <br><br>
     <label for="edit_image">New Image URL:</label>
     <input type="text" name="edit_image" id="edit_image">
     <br><br>
-    <input type="submit" value="Edit">
+    <input class = "button" type="submit" value="Edit">
 </form>
+    </div>
 
 
 
