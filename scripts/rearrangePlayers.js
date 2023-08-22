8525b6807b985307193d50003a01f3f1b91b30ed
Sam Hopkins <shopkins@coretechs.com>
2023-08-22 17:04:27 +0000

    make it so rearrangeplayers regenerates table in new order

--- scripts/rearrangePlayers.js
+++ scripts/rearrangePlayers.js
@@ -13,8 +13,22 @@ function rearrangePlayers(winningPlayerIndex) {
         // Increment the index and wrap around if needed
         i = (i + 1) % playerCount;
     }
-    
-    console.log(order);
+
+    //update order on server
+    $.ajax({
+        url: "scripts/setOrder.php", // Replace with your PHP script's path
+        method: "POST", // Use POST if you're sending data to the server
+        data: {order: order}, // Include your data here
+
+        success: function (response) {
+            // Handle the success response here
+            console.log("Order set successfully:", response);
+        },
+        error: function (xhr, status, error) {
+            // Handle errors here
+            console.error("Error setting order:", error);
+        },
+    });
 
     //generate new table
     generateTable(order);
