5dfb00484fc572c52d07d8a3e4169747c5d2f2df
Sam Hopkins <shopkins@coretechs.com>
2023-08-22 20:55:17 +0000

    move showConditionalCreateOption into create game page because it is only used on that page

--- create_game_page.php
+++ create_game_page.php
@@ -4,8 +4,32 @@
     <title>Create Game</title>
     <meta name="viewport" content="user-scalable=no">
     <link rel="stylesheet" type="text/css" href="styles/createGame.css">
-    <script type="text/javascript" src="scripts/showConditionalCreateOption.js"></script>
     <script type="text/javascript">
+
+        function showConditionalCreateOption() {
+            var gameType = document.getElementById("game_type").value;
+            var count_form = document.getElementById("count_form");
+            var highscore_form = document.getElementById("highscore_form");
+            highscore_form.style.display = "none";
+            count_form.style.display = "none";
+
+            if (gameType === "Countdown") {
+                count_form.style.display = "block";
+                highscore_form.style.display = "none";
+            } 
+            else if (gameType === "Highscore") {
+                count_form.style.display = "none";
+                highscore_form.style.display = "block";
+            }
+            
+            if (gameType === "Countdown") {
+                document.getElementById("count_label").innerHTML = "Count Down From:";
+            }
+            else if (gameType === "Highscore") {
+                document.getElementById("round_label").innerHTML = "Number of Rounds:";
+            }
+        }
+
         function showPlayerForm() {
             var playerCount = document.getElementById("player_count").value;
             var playerForm = document.getElementById("player_form");
