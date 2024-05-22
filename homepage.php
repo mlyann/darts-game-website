cc9cd51c417c67dd6a806e5171ba39a06f1edf04
GavinO <golson@coretechs.com>
2024-05-22 15:50:54 -0400

    Task 147735 - Setup Darts Dev Server -

--- homepage.php
+++ homepage.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <img id = "logo" src = "https://www.coretechs.com/wp-content/uploads/2020/08/Coretechs_Mark.png"></img>
-    <h1>Welcome To <br>Coretechs Darts!</h1>
+    <h1>Welcome To <br>Coretechs Darts! DEV</h1>
     <ul>
         <li><a id = "gameCreator" href="create_game_page.php">Game Creator</a></li>
         <li><a href="scoring.php">Participate In <br> Current Game</a></li>
