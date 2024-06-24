7b016a325a23f02a20ad0300d0872ca420fa30e0
Ming <myang@coretechs.com>
2024-06-24 11:18:06 -0400

    CSS change th desktop showing, got an idea from Manos, will finish later

--- homepage.php
+++ homepage.php
@@ -3,16 +3,18 @@
 <head>
     <title>Homepage</title>
     <meta name="viewport" content="user-scalable=no">
-    <link rel="stylesheet" type="text/css" href="styles/homepage.css">
+    <!-- 链接到CSS文件 -->
+    <link rel="stylesheet" type="text/css" href="homepage.css">
 </head>
 <body>
-    <img id = "logo" src = "https://www.coretechs.com/wp-content/uploads/2020/08/Coretechs_Mark.png"></img>
+    <img id="logo" src="https://www.coretechs.com/wp-content/uploads/2020/08/Coretechs_Mark.png"></img>
     <h1>Welcome To <br>Coretechs Darts! DEV</h1>
     <ul>
-        <li><a id = "gameCreator" href="create_game_page.php">Game Creator</a></li>
+        <li><a id="gameCreator" href="create_game_page.php">Game Creator</a></li>
         <li><a href="scoring.php">Participate In <br> Current Game</a></li>
         <li><a href="scoreDisplayPage.php">Scoreboard (iPad)</a></li>
         <li><a href="leaderboard_page.php">Leaderboard</a></li>
         <li><a href="register_page.php">Player Management</a></li>
+    </ul>
 </body>
 </html>
