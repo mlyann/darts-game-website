07e10a9c1e8b0889720d32792d7d7fc3ecfb424f
Ming <myang@coretechs.com>
2024-07-13 09:54:20 -0400

    bad words

--- scripts/setOrder.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require 'connect.php';
-    $order = $_POST['order'];
-
-    $setOrderQuery = "UPDATE game_data SET modified_order = '$order';";
-    mysqli_query($conn, $setOrderQuery);
-
-    $conn->close();
-?>
